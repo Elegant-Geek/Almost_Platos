435d12e718cbb38a5f35c942ca454f78f76b6f4b
Jamie Clark <clarkcjamie@gmail.com>
2022-04-09 00:51:36 -0400

    9th ci, no login needed to view users now

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   resources :complaints
   resource :session, only: [:new, :create, :destroy] #note singular form "resource and session. SINGULAR session"
- 
+  root "users#index"
   get "login" => "sessions#new"
   get "signup" => "users#new"
   resources :users do
     resources :items
   end
-  root "users#index"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
