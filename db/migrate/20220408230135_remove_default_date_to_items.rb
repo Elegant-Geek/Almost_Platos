f9bc2dd2b5b8a70bdb69b581da64f3b8101f67ed
Jamie Clark <clarkcjamie@gmail.com>
2022-04-17 23:29:20 -0400

    test migration remove this file

--- db/migrate/20220408230135_remove_default_date_to_items.rb
+++ db/migrate/20220408230135_remove_default_date_to_items.rb
@@ -1,6 +1,6 @@
 class RemoveDefaultDateToItems < ActiveRecord::Migration[6.1]
   CREATE_TIMESTAMP = 'DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP'
-  def change
+  def change###
     change_column :items, :found_on, CREATE_TIMESTAMP
   end
 end
