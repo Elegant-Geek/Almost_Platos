d577a9d50511fce8f37e5b42e7408442ea304d8e
Jamie Clark <clarkcjamie@gmail.com>
2022-04-17 23:30:00 -0400

    removed second erroneous migration file

--- db/migrate/20220408230135_remove_default_date_to_items.rb
+++
@@ -1,8 +0,0 @@
-class RemoveDefaultDateToItems < ActiveRecord::Migration[6.1]
-  CREATE_TIMESTAMP = 'DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP'
-  def change###
-    change_column :items, :found_on, CREATE_TIMESTAMP
-  end
-end
-
-
